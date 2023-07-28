084ddd1fb3b0552541f6d9d9f30329aa0048e5cc
Adexlexy <oshewolo_martins@yahoo.com>
2023-07-28 23:07:20 +0100

    victory atlast

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,17 +1,18 @@
+/* eslint-disable no-underscore-dangle */
 /* eslint-disable operator-linebreak */
 /* eslint-disable class-methods-use-this */
 export default class Building {
-    constructor(sqft) {
-      this._sqft = sqft;
-      if (
-        this.evacuationWarningMessage === undefined &&
+  constructor(sqft) {
+    this._sqft = sqft;
+    if (
+      this.evacuationWarningMessage === undefined &&
         this.constructor !== Building
-      ) {
-        throw new Error('Class extending Building must override evacuationWarningMessage');
-      }
-    }
-
-    get sqft() {
-      return this._sqft;
+    ) {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
     }
   }
+
+  get sqft() {
+    return this._sqft;
+  }
+}
--- 0x02-ES6_classes/7-airport.js
+++ 0x02-ES6_classes/7-airport.js
@@ -1,10 +1,12 @@
+/* eslint-disable no-underscore-dangle */
 export default class Airport {
-    constructor(name, code) {
-      this._name = name;
-      this._code = code;
-    }
-
-    toString() {
-      return `[object ${this._code}]`;
-    }
+  constructor(name, code) {
+    // eslint-disable-next-line no-underscore-dangle
+    this._name = name;
+    this._code = code;
   }
+
+  toString() {
+    return `[object ${this._code}]`;
+  }
+}
